acb625f3a7c45aff9c1a2d1f0b598b6a18da661f
aadarshkumaran <aadarshkumar91@gmail.com>
2022-11-19 22:58:49 +0530

    test

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -146,7 +146,7 @@ export const Menu = () => {
             <Item>
             About Press Copyright Contact us Creators Advertise Developers 
             Terms Privacy Policy & Safety How YouTube works Test new features
-            © 2022 Google LLC
+            © 2022 Google LLC.
             </Item>
         </Wrapper>
     </Container>
