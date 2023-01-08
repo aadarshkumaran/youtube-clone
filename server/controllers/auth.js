3b607461c143b7922a6f7c845bdc3f3ed84eae8c
aadarshkumaran <aadarshkumar91@gmail.com>
2023-01-08 12:54:39 +0530

    cor method - 08.01.2023

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,7 +22,6 @@ export const signup = async (req,res,next) =>{
 export const signin = async (req,res,next) =>{
     try {
         const user = await User.findOne({name:req.body.name})
-        console.log(user);
         if(!user) return next(createError(404,"User not found!"))
 
         const isCorrect = await bcrypt.compare(req.body.password,user.password)
